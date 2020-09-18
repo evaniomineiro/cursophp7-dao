378050a4401761e37624a99b415df55116e277d8
Evanio Giraldi Magalhaes <evg_magalhaes@magazineluiza.intranet>
2020-09-18 14:22:18 -0300

    evolução das classes

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 require_once("config.php");
 
-$sql = new Sql();
-$usuarios = $sql->select("SELECT * FROM tb_usuarios;");
+$root = new Usuario();
+$root->loadById(3);
+echo $root;
 
-echo json_encode($usuarios);
 ?>
